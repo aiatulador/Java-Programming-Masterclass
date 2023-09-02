f668742e14b31167fa48707a444000cb10f878bd
Aiatul Al-amin Ador <aiatulador75@gmail.com>
2023-09-02 15:11:17 +0600

    Initial commit

---
+++ src/Expressions_Statements/IfThenElseChallenge.java
@@ -0,0 +1,18 @@
+package Expressions_Statements;
+
+public class IfThenElseChallenge {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 10_000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+        int finalScore = score;
+
+        if(gameOver){
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your Final Score Was: " + finalScore);
+        }
+
+    }
+}
